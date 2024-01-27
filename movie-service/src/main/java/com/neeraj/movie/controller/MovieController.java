67bdcdc7bd5a5955f006d8060ece198ab4459954
neerajrajak <neeraj92.rajak@gmail.com>
2024-01-27 17:10:15 +0530

    service integrated with ui

--- movie-service/src/main/java/com/neeraj/movie/controller/MovieController.java
+++ movie-service/src/main/java/com/neeraj/movie/controller/MovieController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin("*")
 public class MovieController {
 
     @Autowired
--- movie-service/src/main/java/com/neeraj/movie/controller/ReviewController.java
+++ movie-service/src/main/java/com/neeraj/movie/controller/ReviewController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin("*")
 public class ReviewController {
     @Autowired
     private ReviewService service;
